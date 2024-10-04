305a66489af73bb9b7f8e1516b0cd51bbfdeef16
gizawaa <gizawabe3@gmail.com>
2024-10-04 14:19:40 -0400

    creating the other pages

---
+++ frontend/pages/whatTheVidDo/+Page.tsx
@@ -0,0 +1,20 @@
+import { Box, Button, Typography } from '@mui/joy';
+
+export const Page = () => {
+  return (
+    <Box
+      sx={{
+        width: 'fit-content',
+        margin: 'auto',
+        height: '100vh',
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+      }}
+    >
+      <Typography level='h1'>Upload Video</Typography>
+      <Button>Upload</Button>
+    </Box>
+  );
+};
