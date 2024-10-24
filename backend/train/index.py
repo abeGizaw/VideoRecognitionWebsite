6346a0495ff540b3ead76f7349d52decf9fb5a37
abeGizaw <gizawabe3@gmail.com>
2024-10-23 23:04:35 -0400

    bleh

--- backend/train/index.py
+++ backend/train/index.py
@@ -2,7 +2,6 @@ import os
 import pandas as pd
 from joblib import Parallel, delayed
 import pickle
-from trainHelpers import check_video_exists, map_videos_to_labels_joblib, cache_mappings
 
 print('Loading Kinetics-700 dataset...\n')
 annotations_path = './data/kinetics-dataset/k700-2020/annotations'
@@ -13,6 +12,36 @@ val_df = pd.read_csv(val_csv)
 train_videos_dir = './data/kinetics-dataset/k700-2020/train'
 val_videos_dir = './data/kinetics-dataset/k700-2020/val'
 
+def check_video_exists(row, video_dir):
+    label = row['label']
+    time_start = str(row['time_start']).zfill(6)
+    time_end = str(row['time_end']).zfill(6)
+    video_name = f"{row['youtube_id']}_{time_start}_{time_end}.mp4"
+    video_path = os.path.join(video_dir, label, video_name)
+    
+    if os.path.exists(video_path):
+        return video_path, label
+    else:
+        print(f"Warning: Video {video_name} not found in {label} folder.")
+        return None, None
+
+def map_videos_to_labels_joblib(df, video_dir, n_jobs=-1):
+    results = Parallel(n_jobs=n_jobs)(
+        delayed(check_video_exists)(row, video_dir) for _, row in df.iterrows()
+    )
+    video_paths = [res[0] for res in results if res[0] is not None]
+    video_labels = [res[1] for res in results if res[1] is not None]
+    return video_paths, video_labels
+
+def cache_mappings(file_name, data=None):
+    if data is None:  # Load cache
+        if os.path.exists(file_name):
+            with open(file_name, 'rb') as f:
+                return pickle.load(f)
+        return None
+    else:  # Save cache
+        with open(file_name, 'wb') as f:
+            pickle.dump(data, f)
 
 # Load or compute training mappings
 print('Loading training video mappings from cache if available...')
