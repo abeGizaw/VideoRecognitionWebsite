b0214f37b7c216d63279ccd61a1506fe08d31783
rhit-lemonsio <lemonsio@rose-hulman.edu>
2024-10-27 20:37:27 -0400

    reading jester data

--- backend/train/index.py
+++ backend/train/index.py
@@ -14,38 +14,66 @@ val_videos_dir = os.path.join(current_dir, '../../../data/kinetics-dataset/k700-
 train_df = pd.read_csv(train_csv)
 val_df = pd.read_csv(val_csv)
 
-print("Loading Jester dataset...\n")
-current_dir = os.path.dirname(os.path.abspath(__file__))
-jester_path = os.path.join(current_dir, '../../../data/jester')
+
 
 
 # Load or compute training mappings
 print('Loading training video mappings from cache if available...')
-train_video_paths, train_video_labels = load_training_data(train_df, train_videos_dir)
+train_video_paths, train_video_labels = load_training_data(train_df, train_videos_dir, "kinetics")
 
 #test
 # Load or compute validation mappings
 print('Loading validation video mappings from cache if available...')
-val_video_paths, val_video_labels = load_validation_data(val_df, val_videos_dir)
+val_video_paths, val_video_labels = load_validation_data(val_df, val_videos_dir, "kinetics")
 
 
+print("Loading Jester dataset...\n")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+jester_path = os.path.join(current_dir, '../../../data/jester')
+jester_path_train = os.path.join(jester_path, 'jester-v1-train.csv') 
+jester_path_validation = os.path.join(jester_path, 'jester-v1-validation.csv') 
+jester_path_videos =  os.path.join(jester_path,'20bn-jester-v1')
+
+
+# Load or compute training mappings
+jester_training_labels_df = pd.read_csv(jester_path_train)
+jester_validation_labels_df = pd.read_csv(jester_path_validation)
+jesterVideo_train_paths, jester_train_video_labels = load_training_data(jester_training_labels_df,jester_path_videos, "jester")
+
+#Load or compute validation mappings
+jester_validation_labels_df = pd.read_csv(jester_path_validation)
+jesterVideo_val_paths, jester_val_video_labels = load_training_data(jester_validation_labels_df,jester_path_videos, "jester")
+
+
+#Jester Data
+jester_train_video_labels = pd.Series(jester_train_video_labels)
+jester_val_video_labels = pd.Series(jester_val_video_labels)
+
+
+#Kinetics Data
 train_video_labels_series = pd.Series(train_video_labels)
 val_video_labels_series = pd.Series(val_video_labels)
 
 # Returns a Series object with the counts of each unique value in the Series and the unique values
 train_label_counts = train_video_labels_series.value_counts()
 unique_train_labels = train_label_counts.index
-print(f"Unique training labels: {len(unique_train_labels)}")
+print(f"Unique training labels in Kinetics: {len(unique_train_labels)}")
 
+
+# Kinetics things
 most_common_train_label = train_label_counts.idxmax()
 least_common_train_label = train_label_counts.idxmin()
 most_common_train_count = train_label_counts.max()
 least_common_train_count = train_label_counts.min()
 most_common_train_proportion = most_common_train_count / len(train_video_labels)
-print(f"Most common training label: {most_common_train_label} with {most_common_train_count} videos out of {len(train_video_labels_series)}.")
-print(f"Least common training label: {least_common_train_label} with {least_common_train_count} videos out of {len(train_video_labels_series)}.")
-print(f"Proportion of most common training label: {most_common_train_proportion} \n")
+print(f"Kinetics most common training label: {most_common_train_label} with {most_common_train_count} videos out of {len(train_video_labels_series)}.")
+print(f"Kinetics least common training label: {least_common_train_label} with {least_common_train_count} videos out of {len(train_video_labels_series)}.")
+print(f"Kinetics proportion of most common training label: {most_common_train_proportion} \n")
 
+
+
+
+#Kinetics
 val_label_counts = val_video_labels_series.value_counts()
 unique_val_labels = val_label_counts.index
 print(f"Unique validation labels: {len(unique_val_labels)}")
@@ -59,5 +87,43 @@ print(f"Most common validation label: {most_common_val_label} with {most_common_
 print(f"Least common validation label: {least_common_val_label} with {least_common_val_count} videos out of {len(val_video_labels_series)}.")
 print(f"Proportion of most common validation label: {most_common_val_proportion} \n")
 
+
+
+#Jester 
+
+
+# Jester Train
+jester_train_label_counts = jester_train_video_labels.value_counts()
+jester_unique_train_labels = jester_train_label_counts.index
+print(f"Unique training labels in Jester: {len(jester_unique_train_labels)}")
+jester_most_common_train_label = jester_train_label_counts.idxmax()
+jester_least_common_train_label = jester_train_label_counts.idxmin()
+jester_most_common_train_count = jester_train_label_counts.max()
+jester_least_common_train_count = jester_train_label_counts.min()
+jester_most_common_train_proportion = jester_most_common_train_count / len(jester_train_video_labels)
+print(f"Jester most common training label: {jester_most_common_train_label} with {jester_most_common_train_count} videos out of {len(jester_train_label_counts)}.")
+print(f"Jester least common training label: {jester_least_common_train_label} with {jester_least_common_train_count} videos out of {len(jester_train_label_counts)}.")
+print(f"Jester proportion of most common training label: {jester_most_common_train_proportion} \n")
+
+
+# Jester Val
+jester_val_label_counts = jester_val_video_labels.value_counts()
+jester_unique_val_labels = jester_val_label_counts.index
+print(f"Unique training labels in Jester: {len(jester_unique_val_labels)}")
+
+
+jester_most_common_val_label = jester_val_label_counts.idxmax()
+jester_least_common_val_label = jester_val_label_counts.idxmin()
+jester_most_common_val_count = jester_val_label_counts.max()
+jester_least_common_val_count = jester_val_label_counts.min()
+jester_most_common_val_proportion = jester_most_common_val_count / len(jester_val_video_labels)
+print(f"Jester most common training label: {jester_most_common_val_label} with {jester_most_common_val_count} videos out of {len(jester_val_label_counts)}.")
+print(f"Jester least common training label: {jester_least_common_val_label} with {jester_least_common_val_count} videos out of {len(jester_val_label_counts)}.")
+print(f"Jester proportion of most common training label: {jester_most_common_val_proportion} \n")
+
+
+
+#Combination
+print(f"Total unique labels: {len(jester_unique_train_labels) +len(unique_train_labels)}")
 
 
