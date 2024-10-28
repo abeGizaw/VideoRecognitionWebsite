6c1d667e0e3ac9a61c97cfd3712dd0e64601d67b
abeGizaw <gizawabe3@gmail.com>
2024-10-27 21:45:40 -0400

    loading jester data is a success!

--- backend/train/index.py
+++ backend/train/index.py
@@ -16,17 +16,19 @@ val_videos_dir = os.path.join(current_dir, '../../../data/kinetics-dataset/k700-
 train_df = pd.read_csv(train_csv)
 val_df = pd.read_csv(val_csv)
 
+# Print the columns of the dataframes
+print("Columns in train_df:", train_df.columns)
+print("Columns in val_df:", val_df.columns)
 
 
+# # Load or compute training mappings
+# print('Loading training video mappings from cache if available...')
+# train_video_paths, train_video_labels = load_training_data(train_df, train_videos_dir, "kinetics")
 
-# Load or compute training mappings
-print('Loading training video mappings from cache if available...')
-train_video_paths, train_video_labels = load_training_data(train_df, train_videos_dir, "kinetics")
-
-#test
-# Load or compute validation mappings
-print('Loading validation video mappings from cache if available...')
-val_video_paths, val_video_labels = load_validation_data(val_df, val_videos_dir, "kinetics")
+# #test
+# # Load or compute validation mappings
+# print('Loading validation video mappings from cache if available...')
+# val_video_paths, val_video_labels = load_validation_data(val_df, val_videos_dir, "kinetics")
 
 
 print("Loading Jester dataset...\n")
@@ -37,62 +39,58 @@ jester_path_validation = os.path.join(jester_path, 'jester-v1-validation.csv')
 jester_path_videos =  os.path.join(jester_path,'20bn-jester-v1')
 
 
-# Load or compute training mappings
-jester_training_labels_df = pd.read_csv(jester_path_train)
-jester_validation_labels_df = pd.read_csv(jester_path_validation)
+# Load or compute training mappings with specified column names
+jester_training_labels_df = pd.read_csv(jester_path_train, header=None, names=['id', 'label'], sep=';')
+# Load validation data with specified column names
+jester_validation_labels_df = pd.read_csv(jester_path_validation, header=None, names=['id', 'label'], sep = ';')
+
+
 jesterVideo_train_paths, jester_train_video_labels = load_training_data(jester_training_labels_df,jester_path_videos, "jester")
+jesterVideo_val_paths, jester_val_video_labels = load_validation_data(jester_validation_labels_df,jester_path_videos, "jester")
 
-#Load or compute validation mappings
-jester_validation_labels_df = pd.read_csv(jester_path_validation)
-jesterVideo_val_paths, jester_val_video_labels = load_training_data(jester_validation_labels_df,jester_path_videos, "jester")
+# #Kinetics Data
+# train_video_labels_series = pd.Series(train_video_labels)
+# val_video_labels_series = pd.Series(val_video_labels)
 
-
-#Jester Data
-jester_train_video_labels = pd.Series(jester_train_video_labels)
-jester_val_video_labels = pd.Series(jester_val_video_labels)
-
-
-#Kinetics Data
-train_video_labels_series = pd.Series(train_video_labels)
-val_video_labels_series = pd.Series(val_video_labels)
-
-# Returns a Series object with the counts of each unique value in the Series and the unique values
-train_label_counts = train_video_labels_series.value_counts()
-unique_train_labels = train_label_counts.index
-print(f"Unique training labels in Kinetics: {len(unique_train_labels)}")
+# # Returns a Series object with the counts of each unique value in the Series and the unique values
+# train_label_counts = train_video_labels_series.value_counts()
+# unique_train_labels = train_label_counts.index
+# print(f"Unique training labels in Kinetics: {len(unique_train_labels)}")
 
 
-# Kinetics things
-most_common_train_label = train_label_counts.idxmax()
-least_common_train_label = train_label_counts.idxmin()
-most_common_train_count = train_label_counts.max()
-least_common_train_count = train_label_counts.min()
-most_common_train_proportion = most_common_train_count / len(train_video_labels)
-print(f"Kinetics most common training label: {most_common_train_label} with {most_common_train_count} videos out of {len(train_video_labels_series)}.")
-print(f"Kinetics least common training label: {least_common_train_label} with {least_common_train_count} videos out of {len(train_video_labels_series)}.")
-print(f"Kinetics proportion of most common training label: {most_common_train_proportion} \n")
+# # Kinetics things
+# most_common_train_label = train_label_counts.idxmax()
+# least_common_train_label = train_label_counts.idxmin()
+# most_common_train_count = train_label_counts.max()
+# least_common_train_count = train_label_counts.min()
+# most_common_train_proportion = most_common_train_count / len(train_video_labels)
+# print(f"Kinetics most common training label: {most_common_train_label} with {most_common_train_count} videos out of {len(train_video_labels_series)}.")
+# print(f"Kinetics least common training label: {least_common_train_label} with {least_common_train_count} videos out of {len(train_video_labels_series)}.")
+# print(f"Kinetics proportion of most common training label: {most_common_train_proportion} \n")
 
 
 
 
-#Kinetics
-val_label_counts = val_video_labels_series.value_counts()
-unique_val_labels = val_label_counts.index
-print(f"Unique validation labels: {len(unique_val_labels)}")
+# #Kinetics
+# val_label_counts = val_video_labels_series.value_counts()
+# unique_val_labels = val_label_counts.index
+# print(f"Unique validation labels: {len(unique_val_labels)}")
 
-most_common_val_label = val_label_counts.idxmax()
-least_common_val_label = val_label_counts.idxmin()
-most_common_val_count = val_label_counts.max()
-least_common_val_count = val_label_counts.min()
-most_common_val_proportion = most_common_val_count / len(val_video_labels)
-print(f"Most common validation label: {most_common_val_label} with {most_common_val_count} videos out of {len(val_video_labels_series)}.")
-print(f"Least common validation label: {least_common_val_label} with {least_common_val_count} videos out of {len(val_video_labels_series)}.")
-print(f"Proportion of most common validation label: {most_common_val_proportion} \n")
+# most_common_val_label = val_label_counts.idxmax()
+# least_common_val_label = val_label_counts.idxmin()
+# most_common_val_count = val_label_counts.max()
+# least_common_val_count = val_label_counts.min()
+# most_common_val_proportion = most_common_val_count / len(val_video_labels)
+# print(f"Most common validation label: {most_common_val_label} with {most_common_val_count} videos out of {len(val_video_labels_series)}.")
+# print(f"Least common validation label: {least_common_val_label} with {least_common_val_count} videos out of {len(val_video_labels_series)}.")
+# print(f"Proportion of most common validation label: {most_common_val_proportion} \n")
 
 
 
 #Jester 
-
+#Jester Data
+jester_train_video_labels = pd.Series(jester_train_video_labels)
+jester_val_video_labels = pd.Series(jester_val_video_labels)
 
 # Jester Train
 jester_train_label_counts = jester_train_video_labels.value_counts()
@@ -111,7 +109,7 @@ print(f"Jester proportion of most common training label: {jester_most_common_tra
 # Jester Val
 jester_val_label_counts = jester_val_video_labels.value_counts()
 jester_unique_val_labels = jester_val_label_counts.index
-print(f"Unique training labels in Jester: {len(jester_unique_val_labels)}")
+print(f"Unique validation labels in Jester: {len(jester_unique_val_labels)}")
 
 
 jester_most_common_val_label = jester_val_label_counts.idxmax()
@@ -119,13 +117,13 @@ jester_least_common_val_label = jester_val_label_counts.idxmin()
 jester_most_common_val_count = jester_val_label_counts.max()
 jester_least_common_val_count = jester_val_label_counts.min()
 jester_most_common_val_proportion = jester_most_common_val_count / len(jester_val_video_labels)
-print(f"Jester most common training label: {jester_most_common_val_label} with {jester_most_common_val_count} videos out of {len(jester_val_label_counts)}.")
-print(f"Jester least common training label: {jester_least_common_val_label} with {jester_least_common_val_count} videos out of {len(jester_val_label_counts)}.")
-print(f"Jester proportion of most common training label: {jester_most_common_val_proportion} \n")
+print(f"Jester most common validation label: {jester_most_common_val_label} with {jester_most_common_val_count} videos out of {len(jester_val_label_counts)}.")
+print(f"Jester least common vavalidation label: {jester_least_common_val_label} with {jester_least_common_val_count} videos out of {len(jester_val_label_counts)}.")
+print(f"Jester proportion of most common validation label: {jester_most_common_val_proportion} \n")
 
 
 
 #Combination
-print(f"Total unique labels: {len(jester_unique_train_labels) +len(unique_train_labels)}")
+# print(f"Total unique labels: {len(jester_unique_train_labels) +len(unique_train_labels)}")
 
 
--- backend/train/trainHelper.py
+++ backend/train/trainHelper.py
@@ -4,53 +4,63 @@ from joblib import Parallel, delayed
 import pickle
  
 def load_training_data(train_df, train_videos_dir, cache_name):
-    train_mappings = cache_mappings(f'{cache_name}.pkl')
+    train_mappings = cache_mappings(f'{cache_name}_train.pkl')
     if train_mappings is not None:
         train_video_paths, train_video_labels = train_mappings
         print(f"Loaded {len(train_video_paths)} training videos from cache.\n")
     else:
         print('Mapping training videos to labels...')
-        train_video_paths, train_video_labels = map_videos_to_labels_joblib(train_df, train_videos_dir)
+        train_video_paths, train_video_labels = map_videos_to_labels_joblib(train_df, train_videos_dir, cache_name=cache_name)
         print(f"Loaded {len(train_video_paths)} training videos.")
         print(f"Train Video Labels: {len(train_video_labels)}")
         print("Caching training video mappings...\n")
-        cache_mappings(f'{cache_name}.pkl', (train_video_paths, train_video_labels))
+        cache_mappings(f'{cache_name}_train.pkl', (train_video_paths, train_video_labels))
  
     return train_video_paths, train_video_labels
  
  
-def load_validation_data(val_df, val_videos_dir):
-    val_mappings = cache_mappings('val_cache.pkl')
+def load_validation_data(val_df, val_videos_dir, cache_name):
+    val_mappings = cache_mappings(f'{cache_name}_val.pkl')
     if val_mappings is not None:
         val_video_paths, val_video_labels = val_mappings
         print(f"Loaded {len(val_video_paths)} validation videos from cache.\n")
     else:
         print('Mapping validation videos to labels...')
-        val_video_paths, val_video_labels = map_videos_to_labels_joblib(val_df, val_videos_dir)
+        val_video_paths, val_video_labels = map_videos_to_labels_joblib(val_df, val_videos_dir, cache_name = cache_name)
         print(f"Loaded {len(val_video_paths)} validation videos.")
         print(f"Val Video Labels: {len(val_video_labels)}")
         print("Caching validation video mappings...\n")
-        cache_mappings('val_cache.pkl', (val_video_paths, val_video_labels))
+        cache_mappings(f'{cache_name}_val.pkl', (val_video_paths, val_video_labels))
  
     return val_video_paths, val_video_labels
  
  
-def check_video_exists(row, video_dir):
-    label = row['label']
-    time_start = str(row['time_start']).zfill(6)
-    time_end = str(row['time_end']).zfill(6)
-    video_name = f"{row['youtube_id']}_{time_start}_{time_end}.mp4"
-    video_path = os.path.join(video_dir, label, video_name)
-   
-    if os.path.exists(video_path):
-        return video_path, label
-    else:
-        print(f"Warning: Video {video_name} not found in {label} folder.")
-        return None, None
+def check_video_exists(row, video_dir, cache_name):
+    if cache_name == 'jester':
+        label = row['label']
+        folder_name = row['id']
+        folder_path = os.path.join(video_dir, str(folder_name))
+        if os.path.exists(folder_path):
+            return folder_path, label
+        else: 
+            print(f"Warning: Folder {folder_path} not found in {video_dir} folder.")
+    else: 
+        label = row['label']
+        time_start = str(row['time_start']).zfill(6)
+        time_end = str(row['time_end']).zfill(6)
+        video_name = f"{row['youtube_id']}_{time_start}_{time_end}.mp4"
+        video_path = os.path.join(video_dir, label, video_name)
+    
+        if os.path.exists(video_path):
+            return video_path, label
+        else: 
+            print(f"Warning: Video {video_name} not found in {label} folder.")
+    return None, None
  
-def map_videos_to_labels_joblib(df, video_dir, n_jobs=-1):
+def map_videos_to_labels_joblib(df, video_dir, n_jobs=-1,*, cache_name=None):
+    # sife of df
     results = Parallel(n_jobs=n_jobs)(
-        delayed(check_video_exists)(row, video_dir) for _, row in df.iterrows()
+        delayed(check_video_exists)(row, video_dir, cache_name) for _, row in df.iterrows()
     )
     video_paths = [res[0] for res in results if res[0] is not None]
     video_labels = [res[1] for res in results if res[1] is not None]
